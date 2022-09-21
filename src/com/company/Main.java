93dc77beeb6e03253f634098a895ec53edb40ff1
pgopika <paripallygopika@gmail.com>
2022-09-21 12:07:32 +0530

    intial

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("im writing from intelliji ! ");
+        System.out.println("changes made after adding into vcs ! ");
+
+    }
+}
