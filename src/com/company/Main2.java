29f4c28128e241f487db6e60d27a83222b65bf0e
paripallygopika <112694867+paripallygopika@users.noreply.github.com>
2022-09-21 12:47:31 +0530

    Update Main2.java

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -6,6 +6,7 @@ public class Main2 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("secong main ! ");
+        // im making chnages from remote respo !!!
 
     }
 }
