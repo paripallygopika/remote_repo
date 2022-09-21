a76ad0360dda17011d0c9727c803b0290771bd44
pgopika <paripallygopika@gmail.com>
2022-09-21 12:56:43 +0530

    commit 2

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -6,6 +6,6 @@ public class Main2 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("secong main ! ");
-
+       // i added from local respo
     }
 }
