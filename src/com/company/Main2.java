826d4267621d009bdd83b0ba50eba5443f520cca
paripallygopika <112694867+paripallygopika@users.noreply.github.com>
2022-09-23 16:17:29 +0530

    Update Main2.java

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -7,5 +7,6 @@ public class Main2 {
         // write your code here
         System.out.println("secong main ! ");
 // i made changes from local repo
+        // 4.17pm i updated from remote repo 
     }
 }
